259c3fc1a9fee88b72ded5a55e37aba8336cf0a9
Guilherme Martini <guilherme.martini20@gmail.com>
2025-01-31 14:45:01 -0300

    set customer variable

--- lib/stripe_mock/request_handlers/customers.rb
+++ lib/stripe_mock/request_handlers/customers.rb
@@ -56,7 +56,7 @@ module StripeMock
           add_coupon_to_object(customers[stripe_account][params[:id]], coupon)
         end
 
-        customers[stripe_account][params[:id]]
+        customer = customers[stripe_account][params[:id]]
         expand_params(customer, params)
       end
 
