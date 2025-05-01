bf08d0c8a632c2366a5b94bee18b0e79dc49e8b1
Guilherme Martini <guilherme.martini20@gmail.com>
2025-05-01 16:58:59 -0300

    add support for updating a coupon

--- lib/stripe_mock/request_handlers/coupons.rb
+++ lib/stripe_mock/request_handlers/coupons.rb
@@ -4,6 +4,7 @@ module StripeMock
 
       def Coupons.included(klass)
         klass.add_handler 'post /v1/coupons',        :new_coupon
+        klass.add_handler 'post /v1/coupons/(.*)',   :update_coupon
         klass.add_handler 'get /v1/coupons/(.*)',    :get_coupon
         klass.add_handler 'delete /v1/coupons/(.*)', :delete_coupon
         klass.add_handler 'get /v1/coupons',         :list_coupons
@@ -14,6 +15,14 @@ module StripeMock
         raise Stripe::InvalidRequestError.new('Missing required param: duration', 'coupon', http_status: 400) unless params[:duration]
         raise Stripe::InvalidRequestError.new('You must pass currency when passing amount_off', 'coupon', http_status: 400) if params[:amount_off] && !params[:currency]
         coupons[ params[:id] ] = Data.mock_coupon({amount_off: nil, percent_off:nil}.merge(params))
+      end
+
+      def update_coupon(route, method_url, params, headers)
+        route =~ method_url
+        assert_existence :coupon, $1, coupons[$1]
+        allowed_params = params.slice(:metadata, :name, :currency_options)
+        coupons[$1].merge!(allowed_params)
+        coupons[$1]
       end
 
       def get_coupon(route, method_url, params, headers)
